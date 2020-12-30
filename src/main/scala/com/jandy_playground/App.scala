a36bade24cba33a90a782adac2a701c4b272fb9f
JandyTenedora <jandy.tenedora@hotmail.com>
2020-12-31 01:14:48 +1100

    Adding Spark Session

--- src/main/scala/com/jandy_playground/App.scala
+++ src/main/scala/com/jandy_playground/App.scala
@@ -1,4 +1,6 @@
 package com.jandy_playground;
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 
 /**
  * @author ${user.name}
@@ -6,7 +8,13 @@ package com.jandy_playground;
 object App {
   
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
+  val spark: SparkSession = SparkSession.builder()
+    .master("local[3]")
+    .appName("JandysSparkApplication")
+    .getOrCreate()
+  val sc = spark.sparkContext
+
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
